54f240b8d9c602ddddce6a208d7d5d68a9f57b06
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-07 22:05:42 +0300

    fix Routr

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
+import { PrivateRoute, PublickRoute } from 'hook/Route';
+import { AuthNavigation } from 'page/AuthNavigation/AuthNavigation';
+import { Authorization } from 'page/AuthNavigation/Authorization/Authorization';
+import { Registration } from 'page/AuthNavigation/Registration/Registration';
+import Home from 'page/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<AuthNavigation />}>
+          <Route
+            path="authorization"
+            element={
+              <PublickRoute redirectTo="/home" component={<Authorization />} />
+            }
+          />
+          <Route
+            path="registration"
+            element={
+              <PublickRoute redirectTo="/home" component={<Registration />} />
+            }
+          />
+        </Route>
+
+        <Route
+          path="/home"
+          element={<PrivateRoute redirectTo="/" component={<Home />} />}
+        >
+          <Route
+            path="userInfo"
+            element={
+              <PrivateRoute redirectTo="/" component={<div>userInfo</div>} />
+            }
+          />
+          <Route
+            path="phonebook"
+            element={
+              <PrivateRoute redirectTo="/" component={<div>phonebook</div>} />
+            }
+          />
+        </Route>
+      </Routes>
     </div>
   );
 };

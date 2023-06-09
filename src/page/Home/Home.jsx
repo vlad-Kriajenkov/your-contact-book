aadaa1f118a137d65aa4bccbbe566bbfa5745c1e
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-09 23:49:05 +0300

    Update Home.jsx

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,13 +1,26 @@
-import HomeBar from "components/HomeBar/HomeBar";
-import { Outlet } from "react-router-dom";
-import { Box } from "./Home.styled";
+import HomeBar from 'components/HomeBar/HomeBar';
+import { Outlet } from 'react-router-dom';
+import { Box } from './Home.styled';
+import { Container } from '@mui/material';
+import HeaderInfo from 'components/HeaderInfo/HeaderInfo';
+import { useSelector } from 'react-redux';
+import { selecrotFetching } from 'redux/auth/auth-selector';
 
 const Home = () => {
-    return(
-        <Box>
-            <HomeBar/>
-            <Outlet/>
-        </Box>
-    )
+  const isFetchingCurrentUser = useSelector(selecrotFetching);
+  return (
+    <Box>
+      <HomeBar />
+      <Container maxWidth="xl">
+        {!isFetchingCurrentUser && (
+          <>
+          
+            <HeaderInfo />
+            <Outlet />
+          </>
+        )}
+      </Container>
+    </Box>
+  );
 };
 export default Home;

91527913f11e9714fd583618abdb8dcd4690a391
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-09 23:48:57 +0300

    Create AddContact.styled.jsx

---
+++ src/page/Phonebook/AddContact/AddContact.styled.jsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import { Grid } from '@mui/material';
+
+export const ContainerForm = styled.div`
+  height: fit-content;
+  width: fit-content;
+
+  padding: 20px;
+  margin-right: 30px;
+  background-color: #ffffff;
+  box-shadow: 0px 1px 4px rgba(126, 147, 255, 0.2);
+  border-radius: 8px;
+`;
+
+export const Title = styled.h1`
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 120%;
+
+  margin-bottom: 20px;
+`;
+
+export const InputBox = styled(Grid)`
+  margin-bottom: 20px;
+`;
+ 

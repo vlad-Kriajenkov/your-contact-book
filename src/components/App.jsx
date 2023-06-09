ca17eefe4523ced73f8dc17b666b8efa79af0b83
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-06-09 23:49:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,20 @@ import { AuthNavigation } from 'page/AuthNavigation/AuthNavigation';
 import { Authorization } from 'page/AuthNavigation/Authorization/Authorization';
 import { Registration } from 'page/AuthNavigation/Registration/Registration';
 import Home from 'page/Home/Home';
+import PhoneBook from 'page/Phonebook/Phonebook';
+import UserInfo from 'page/UserInfo/UserInfo';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import { fetchCurrentUser } from 'redux/auth/auth-operation';
 
 export const App = () => {
+const dispatch = useDispatch()
+
+useEffect(()=>{
+  dispatch(fetchCurrentUser())
+},[dispatch])
+
   return (
     <div>
       <Routes>
@@ -13,13 +24,19 @@ export const App = () => {
           <Route
             path="authorization"
             element={
-              <PublickRoute redirectTo="/home" component={<Authorization />} />
+              <PublickRoute
+                redirectTo="/home/userInfo"
+                component={<Authorization />}
+              />
             }
           />
           <Route
             path="registration"
             element={
-              <PublickRoute redirectTo="/home" component={<Registration />} />
+              <PublickRoute
+                redirectTo="/home/userInfo"
+                component={<Registration />}
+              />
             }
           />
         </Route>
@@ -31,13 +48,13 @@ export const App = () => {
           <Route
             path="userInfo"
             element={
-              <PrivateRoute redirectTo="/" component={<div>userInfo</div>} />
+              <PrivateRoute redirectTo="/" component={<UserInfo/>} />
             }
           />
           <Route
             path="phonebook"
             element={
-              <PrivateRoute redirectTo="/" component={<div>phonebook</div>} />
+              <PrivateRoute redirectTo="/" component={<PhoneBook/>} />
             }
           />
         </Route>
